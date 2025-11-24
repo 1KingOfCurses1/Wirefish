6d0cef86e334b7e3c8ccdb316e6ef7e74eb1f608
rinnegannn <aryanv1945@gmail.com>
2025-11-24 12:49:53 -0500

    Added guard rails

--- net/net.h
+++ net/net.h
@@ -14,6 +14,9 @@
  * Aryan Verma, 400575438, McMaster University
  */
 
+#ifndef NET_H
+#define NET_H
+
 #include <sys/socket.h>
 #include <netinet/in.h>
 
@@ -21,3 +24,5 @@ int net_resolve(const char *host, struct sockaddr_storage *out, socklen_t *outle
 int net_tcp_connect(const struct sockaddr *sa, socklen_t slen, int timeout_ms);
 int net_set_ttl(int sockfd, int ttl);
 int net_icmp_raw_socket();
+
+#endif 
