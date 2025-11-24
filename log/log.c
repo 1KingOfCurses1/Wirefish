cc8e82fd623589a8e8e527446050d44ca1f1bc80
youssefkhafagy <youssef.m.khafagy@gmail.com>
2025-11-23 23:25:04 -0500

    Adding log and model files

--- log/log.c
+++ log/log.c
@@ -2,3 +2,94 @@
  * File: log.c
  * Implements printf-style logging to stderr with level filtering.
  */
+
+#include <stdio.h>
+#include <stdarg.h>
+#include "log.h"
+
+
+//Starting level
+static LogLevel currentLevel = LOG_INFO;
+
+
+/*
+ * This is what sets what level of log messages we want to show
+ * chosenLevel = the level to use
+ * no return
+ */
+void log_set_level(LogLevel chosenLevel){
+    currentLevel = chosenLevel;
+}
+
+
+/*
+ * helper so we dont type the same code 4 times
+ * messageLevel = level for the message
+ * tag = the text like it can be something like "debug"
+ * message = printf-style message text
+ * args = extra stuff for the format
+ */
+static void write_log_message(LogLevel messageLevel, char *tag, char *message, va_list args){
+
+    //dont print if it's below the level we set
+    if(messageLevel < currentLevel){
+        return;
+    }
+
+    //print tag first
+    fprintf(stderr,"[%s] ", tag);
+
+    //print the message like printf
+    vfprintf(stderr, message, args);
+    
+    //newline so logs dont stick together
+    fprintf(stderr,"\n");
+}
+
+
+
+/*
+ * prints a debug log message
+ */
+void log_debug(char *message, ...){
+    va_list args;
+    va_start(args, message);
+    write_log_message(LOG_DEBUG, "debug : ", message, args);
+    va_end(args);
+}
+
+
+
+/*
+ * prints an info log message
+ */
+void log_info(char *message, ...){
+    va_list args;
+    va_start(args, message);
+    write_log_message(LOG_INFO, "Info : ", message, args);
+    va_end(args);
+}
+
+
+
+/*
+ * prints a warning message
+ */
+void log_warn(char *message, ...){
+    va_list args;
+    va_start(args, message);
+    write_log_message(LOG_WARN, "warn : ", message, args);
+    va_end(args);
+}
+
+
+
+/*
+ * prints an error message
+ */
+void log_error(char *message, ...){
+    va_list args;
+    va_start(args, message);
+    write_log_message(LOG_ERROR, "error : ", message, args);
+    va_end(args);
+}
--- model/model.h
+++ model/model.h
@@ -16,5 +16,53 @@
  */
 #ifndef MODEL_H
 #define MODEL_H
-/* Option 1: re-typedef here (dup types); Option 2: include feature headers */
-#endif /* MODEL_H */
+
+#include <stddef.h>
+#include <stdbool.h>
+
+typedef enum { PORT_CLOSED = 0, PORT_OPEN = 1, PORT_FILTERED = 2 } PortState;
+
+typedef struct {
+    int port;
+    PortState state; //or just int if you haven't switched yet
+    int latency_ms;
+} ScanResult;
+
+typedef struct {
+    ScanResult *rows;
+    size_t len, cap; //note: check with team if size_t is preferred over int since int might be too small for very large scans
+} ScanTable;
+
+typedef struct {
+    int  hop;
+    char host[256];
+    char ip[64];
+    int  rtt_ms;  //-1 if timeout
+    bool timeout;
+} Hop;
+
+typedef struct {
+    Hop *rows;
+    size_t len, cap;
+} TraceRoute;
+
+
+typedef struct {
+
+    char iface[64]; //interface name
+    long rx_bytes; //total received bytes
+    long tx_bytes; //total sent bytes
+    double rx_rate_bps;
+    double tx_rate_bps;
+    double rx_avg_bps;
+    double tx_avg_bps;
+} IfaceStats;
+
+typedef struct {
+    IfaceStats *samples; //array of samples
+    size_t len, cap;   // number stored + capacity
+
+} MonitorSeries;
+
+
+#endif
